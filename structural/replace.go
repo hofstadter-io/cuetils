03e488f416c1695cc07923237ee5304dc0bfa5cd
Tony Worm <tony@hofstadter.io>
2021-11-20 22:52:32 -0500

    implement replace in Go
    Signed-off-by: Tony Worm <tony@hofstadter.io>

--- structural/replace.go
+++ structural/replace.go
@@ -16,42 +16,72 @@ replace: val.replace
 `
 
 func ReplaceGlobs(code string, globs []string, opts *flags.RootPflagpole) ([]GlobResult, error) {
-	return ReplaceGlobsCue(code, globs, opts)
-}
-
-func ReplaceGlobsGo(code string, globs []string, opts *flags.RootPflagpole) ([]GlobResult, error) {
 	return BinaryOpGlobs(code, globs, opts, ReplaceValue)
 }
 
-// TODO, make opts a pointer so we are only passing that during recursion
 func ReplaceValue(repl, val cue.Value, opts *flags.RootPflagpole) (cue.Value, error) {
 	r, _ := replaceValue(repl, val, opts)
 	return r, nil
 }
 
-func replaceValue(repl, val cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
-	switch val.IncompleteKind() {
+func replaceValue(repl, target cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+	switch target.IncompleteKind() {
 	case cue.StructKind:
-		return replaceStruct(repl, val, opts)
+		return replaceStruct(repl, target, opts)
 
 	case cue.ListKind:
-		return replaceList(repl, val, opts)
+		return replaceList(repl, target, opts)
 
 	default:
 		// should already have the same label by now
+		// but maybe not if target is basic and repl is not
 		return repl, true
 	}
-	return val, false
 }
 
-func replaceStruct(repl, from cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+func replaceStruct(repl, target cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+	ctx := target.Context()
 
-	return repl, false
+	result := newStruct(ctx)
+	iter, _ := target.Fields(defaultWalkOptions...)
+
+	cnt := 0
+	for iter.Next() {
+		cnt++
+		s := iter.Selector()
+		p := cue.MakePath(s)
+		r := repl.LookupPath(p)
+		// fmt.Println(cnt, iter.Value(), f, f.Exists())
+		// check that field exists in from. Should we be checking f.Err()?
+		if r.Exists() {
+			v, ok := replaceValue(r, iter.Value(), opts)
+			// fmt.Println("r:", r, ok, p)
+			if ok {
+				result = result.FillPath(p, v)
+			}
+		} else {
+			// include if not in replace
+			result = result.FillPath(p, iter.Value())
+		}
+	}
+
+	return result, true
 }
 
-func replaceList(repl, from cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+func replaceList(repl, target cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+	ctx := target.Context()
 
-	return repl, false
+	ri, _ := repl.List()
+	ti, _ := target.List()
+
+	result := []cue.Value{}
+	for ri.Next() && ti.Next() {
+		r, ok := replaceValue(ri.Value(), ti.Value(), opts)
+		if ok {
+			result = append(result, r)
+		}
+	}
+	return ctx.NewList(result...), true
 }
 
 func ReplaceGlobsCue(code string, globs []string, opts *flags.RootPflagpole) ([]GlobResult, error) {
