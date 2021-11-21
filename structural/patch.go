cbd7b2dd7d02103d04341c8400590572304f72d0
Tony Worm <tony@hofstadter.io>
2021-11-21 00:55:33 -0500

    implement patch in Go
    Signed-off-by: Tony Worm <tony@hofstadter.io>

--- structural/patch.go
+++ structural/patch.go
@@ -1,25 +1,12 @@
 package structural
 
 import (
-	"fmt"
-
 	"cuelang.org/go/cue"
 
 	"github.com/hofstadter-io/cuetils/cmd/cuetils/flags"
 )
 
-const patchfmt = `
-val: #Patch%s
-val: #X: _
-val: #P: _
-patch: val.patch
-`
-
 func PatchGlobs(patch string, orig string, opts *flags.RootPflagpole) ([]GlobResult, error) {
-	return PatchGlobsCue(patch, orig, opts)
-}
-
-func PatchGlobsGo(patch string, orig string, opts *flags.RootPflagpole) ([]GlobResult, error) {
 	return BinaryOpGlobs(patch, []string{orig}, opts, PatchValue)
 }
 
@@ -29,54 +16,66 @@ func PatchValue(patch, val cue.Value, opts *flags.RootPflagpole) (cue.Value, err
 }
 
 func patchValue(patch, val cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
-	return val, false
+	switch val.IncompleteKind() {
+	case cue.StructKind:
+		// fmt.Println("struct", orig, next)
+		return patchStruct(patch, val, opts)
+
+	case cue.ListKind:
+		// fmt.Println("list", orig, next)
+		return patchList(patch, val, opts)
+
+	default:
+		panic("should not get here")
+	}
 }
 
-func PatchGlobsCue(patch string, orig string, opts *flags.RootPflagpole) ([]GlobResult, error) {
-	cuest, err := NewCuest([]string{"patch"}, nil)
-	if err != nil {
-		return nil, err
-	}
+func patchStruct(patch, val cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+	ctx := val.Context()
+	result := newStruct(ctx)
+	rmv := patch.LookupPath(cue.ParsePath("\"-\""))
 
-	operator, err := ReadArg(patch, cuest.ctx, nil)
-	if err != nil {
-		return nil, err
-	}
+	iter, _ := val.Fields(defaultWalkOptions...)
+	for iter.Next() {
+		s := iter.Selector()
+		p := cue.MakePath(s)
+		r := rmv.LookupPath(p)
 
-	inputs, err := LoadGlobs([]string{orig})
-	if len(inputs) == 0 {
-		return nil, fmt.Errorf("no inputs found")
-	}
-
-	// construct reusable val with function
-	maxiter := ""
-	if mi := opts.Maxiter; mi > 0 {
-		maxiter = fmt.Sprintf(" & { #maxiter: %d }", mi)
-	}
-	content := fmt.Sprintf(patchfmt, maxiter)
-	val := cuest.ctx.CompileString(content, cue.Scope(cuest.orig))
-
-	// update val with the orig value
-	val = val.FillPath(cue.ParsePath("val.#P"), operator.Value)
-
-	results := make([]GlobResult, 0)
-	for _, input := range inputs {
-
-		iv := cuest.ctx.CompileBytes(input.Content, cue.Filename(input.Filename))
-		if iv.Err() != nil {
-			return nil, iv.Err()
+		// skip any field which exists, it was the same in the original diff
+		// so if not in remove...
+		if !r.Exists() {
+			v := patch.LookupPath(p)
+			if v.Exists() {
+				// if in patch, we need to recurse
+				r, _ := patchValue(v, iter.Value(), opts)
+				result = result.FillPath(p, r)
+			} else {
+				// keep any field which !exists, it was the same in the original diff
+				result = result.FillPath(p, iter.Value())
+			}
 		}
-
-		result := val.FillPath(cue.ParsePath("val.#X"), iv)
-
-		v := result.LookupPath(cue.ParsePath("patch"))
-
-		results = append(results, GlobResult{
-			Filename: input.Filename,
-			Value:    v,
-		})
-
 	}
 
-	return results, nil
+	add := patch.LookupPath(cue.ParsePath("\"+\""))
+	if add.Exists() {
+		result = result.Unify(add)
+	}
+
+	return result, true
+}
+
+func patchList(patch, val cue.Value, opts *flags.RootPflagpole) (cue.Value, bool) {
+	ctx := val.Context()
+	oi, _ := patch.List()
+	ni, _ := val.List()
+
+	result := []cue.Value{}
+	for oi.Next() && ni.Next() {
+		v, ok := patchValue(oi.Value(), ni.Value(), opts)
+		if ok {
+			result = append(result, v)
+		}
+	}
+
+	return ctx.NewList(result...), true
 }
