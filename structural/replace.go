cce16a40445bb93c68e8b16ca9c86900368e8b2f
Tony Worm <tony@hofstadter.io>
2021-11-20 22:53:34 -0500

    remove old replace implementation
    Signed-off-by: Tony Worm <tony@hofstadter.io>

--- structural/replace.go
+++ structural/replace.go
@@ -1,8 +1,6 @@
 package structural
 
 import (
-	"fmt"
-
 	"cuelang.org/go/cue"
 
 	"github.com/hofstadter-io/cuetils/cmd/cuetils/flags"
@@ -82,53 +80,4 @@ func replaceList(repl, target cue.Value, opts *flags.RootPflagpole) (cue.Value, 
 		}
 	}
 	return ctx.NewList(result...), true
-}
-
-func ReplaceGlobsCue(code string, globs []string, opts *flags.RootPflagpole) ([]GlobResult, error) {
-	cuest, err := NewCuest([]string{"replace"}, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	operator, err := ReadArg(code, cuest.ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	inputs, err := LoadGlobs(globs)
-	if len(inputs) == 0 {
-		return nil, fmt.Errorf("no inputs found")
-	}
-
-	// construct reusable val with function
-	maxiter := ""
-	if mi := opts.Maxiter; mi > 0 {
-		maxiter = fmt.Sprintf(" & { #maxiter: %d }", mi)
-	}
-	content := fmt.Sprintf(replacefmt, maxiter)
-	val := cuest.ctx.CompileString(content, cue.Scope(cuest.orig))
-
-	// fill val with the orig value, so we only need to once before loop
-	val = val.FillPath(cue.ParsePath("val.#R"), operator.Value)
-
-	results := make([]GlobResult, 0)
-	for _, input := range inputs {
-
-		iv := cuest.ctx.CompileBytes(input.Content, cue.Filename(input.Filename))
-		if iv.Err() != nil {
-			return nil, iv.Err()
-		}
-
-		result := val.FillPath(cue.ParsePath("val.#X"), iv)
-
-		v := result.LookupPath(cue.ParsePath("replace"))
-
-		results = append(results, GlobResult{
-			Filename: input.Filename,
-			Value:    v,
-		})
-
-	}
-
-	return results, nil
 }
